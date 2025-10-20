b5bda1c4265561e1f2384ef9bf5e92be8f46ece0
CarNavMar-Ilerna <carlitonavmar@alumnos.ilerna.com>
2025-10-20 21:07:58 +0200

    Nuevos ejercicios

--- Python/Actividades/Listas/21-30.py
+++ Python/Actividades/Listas/21-30.py
@@ -1,2 +1,75 @@
 #Ejercicios del 21 al 30
 
+#21.Crea una lista de números 
+numeros1 = [2, 4, 10, 23, 41]
+
+#Usa el sum() para obtener toda la suma de los elementos
+print(sum(numeros1))
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#22.Crea una lista de booleanos
+booleanos = [True, False, True, False]
+
+#Usa all() para verificar si todos los elementos son true
+print(all(booleanos))
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#23.Usa any() para verificar si almenos uno de los elementos es True
+print(any(booleanos))
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#24.Convierte una lista de cadenas en una cadena unica separada por comas usando join()
+cadenas = ["A", "mi", "no", "me", "apetece"]
+print(",".join(cadenas))
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#25.Usa list() para convertir una cadena en una lista de caracteres
+cadena = "La papaya esta muy buena"
+print(list(cadena))
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#26.Crea una lista con 10 números
+numeros2 = [2, 4, 10, 45, 67, 89, 12, 42, 35, 90]
+
+#Usa slicing para obtener una sublista con los numeros de indice 2 al 6
+numeros_slicing = numeros2[2:6]
+print(numeros_slicing)
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#27.Elimina el último elemento de una lista de ciudades usando pop() con indice negativo
+ciudades = ["Sevilla", "Lisboa", "Berlín", "Milan", "Atenas"]
+ciudades.pop(-1)
+print(ciudades)
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#28.Usa max() para encontrar el valor más grande en una lista de números
+print(max(numeros2))
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#29.Usa min() para encontrar el valor más pequeño en una lista de números
+print(min(numeros2))
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#30.Crea una lista de números al azar
+numeros3 = [34, 1, 67, 89, 9, 34, 23]
+
+#Usa sorted() para ordenar la lista sin modificar la original
+print(sorted(numeros3))
---
+++ Python/Actividades/Listas/31-40.py
@@ -0,0 +1,80 @@
+#Ejercicios del 31 al 40
+
+#31.Crea una lista que contenga números y cadenas
+mezcla = [2, 45, "pizza", 34, "peru"]
+
+#Accede al segundo elemento
+print(mezcla[1])
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#32. Usa enumerate() para imprimir el indice y valor de cada elemento de una lista
+indice = ['patata', 'manzana', 'guayaba']
+indice_enum = enumerate(indice)
+print(list(indice_enum))
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#33.Crea una lista de listas
+listade = [["pollos", "cerdos", "ovejas"],["patatas", "zanahorias", "lechuga"]]
+
+#Usa for para recorrer los elementos de cada lista
+for interior in listade:
+    for x in interior:
+        print(x)
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#34.Usa del para eliminar el rango de elementos de una lista de números
+numeros = [2, 33, 45, 21, 83, 56]
+del numeros[2:4]
+print(numeros)
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#35.Filtra los elementos de una lista de números para obtener solo los 5 mayores
+numeros2 = [2, 33, 45, 21, 83, 56, 38, 23, 90]
+numeros2.sort()
+numeros2.reverse()
+print(numeros2[:5])
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#36.Crea una lista de cadenas y usa split() para dividir cada cadena en palabras
+lista_cadenas = [
+    "Este es un ejemplo",
+    "Otro texto más largo para probar",
+    "Palabras",
+    "Una cadena con  espacios extra"
+]
+for cadena in lista_cadenas:
+    palabras = cadena.split()
+    print(palabras)
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#37.Convierte en tupla en una lista y luego añade un nuevo elemento
+tupla = ("pollo", "conejo", "vaca")
+tupla_lista = list(tupla)
+tupla_lista.append("borrego")
+print(tupla_lista)
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#38.Usa set() para eliminar los elementos duplicados de una lista
+mi_lista = [10, 32, 32, 38, 41, 41, 57, 10]
+lista_sin_duplicados = list(set(mi_lista))
+print(lista_sin_duplicados)
+print("...............................................................................")
+
+#-------------------------------------------------------------------------------------------
+
+#39.Crea una lista de números
+numeros3 =
